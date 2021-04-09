dd436c0383589484cb120576857679f578151379
Jhonathan S Soares <jhonathanmarolo@hotmail.com>
2021-04-09 10:32:05 -0300

    Add tests

--- src/shelf.js
+++ src/shelf.js
@@ -15,25 +15,26 @@ const generateShelfData = (usecases) => {
   return shelfData
 
 }
+
 const formatUseCaseDoc = (usecase) => {
   var requestParams = []
   var responseParams = []
 
   if (usecase.request) {
-    if (Object.entries(usecase.request).length == 0) {
+    if (Object.entries(usecase.request).length == 0)
       responseParams.push({ 'name': 'Object of', 'type': usecase.request.name })
-    }
-    for (const [key, value] of Object.entries(usecase.request)) {
-      requestParams.push({ 'name': key, 'type': value.name ? value.name : `Array of ${value[0].name}` })
+    else {
+      for (const [key, value] of Object.entries(usecase.request)) {
+        requestParams.push({ 'name': key, 'type': value.name ? value.name : `Array of ${value[0].name}` })
 
+      }
     }
     usecase.request = requestParams
   }
 
   if (usecase.response) {
-    if (Object.entries(usecase.response).length == 0) {
+    if (Object.entries(usecase.response).length == 0)
       responseParams.push({ 'name': 'Object of', 'type': usecase.response.name })
-    }
     else {
       for (const [key, value] of Object.entries(usecase.response)) {
         responseParams.push({ 'name': key == '0' ? 'Array of' : key, 'type': value.name })
--- test/shelf.test.js
+++ test/shelf.test.js
@@ -37,5 +37,79 @@ describe("Generate usecase self-documentation", () => {
 
 
   })
+
+  describe('the simplest use case with complex request and response', () => {
+
+    var usecases = []
+    const givenTheSimplestUseCaseWithRequestResponse = () => {
+      const uc = usecase('A use case', {
+        request: {
+          param1: String,
+          param2: Number
+        },
+        response: {
+          output1: String
+        },
+        'A step': step((ctx) => {
+          ctx.ret.response3 = ctx.req.param2 + 1
+          return Ok()
+        })
+      })
+      return uc
+    }
+
+    it('should generate some doc', () => {
+      //given
+      usecases.push({ usecase: givenTheSimplestUseCaseWithRequestResponse(), tags: { group: 'SimplestestUseCaseWithRequestResponse' } },)
+      var shelf = renderShelfHTML(usecases)
+      //then
+      assert.ok(shelf)
+    })
+
+    it('should validate if documentation generated is a valid HTML document', () => {
+      //given
+      var shelf = renderShelfHTML(usecases)
+      //then
+      assert.strictEqual(/<(?=.*? .*?\/ ?>|br|hr|input|!--|wbr)[a-z]+.*?>|<([a-z]+).*?<\/\1>/i.test(shelf), true)
+
+    })
+
+
+
+  })
+
+  describe('the simplest use case with request and response without name declaration', () => {
+
+    var usecases = []
+    const givenTheSimplestUseCaseWithRequestResponse = () => {
+      const uc = usecase('A use case', {
+        request: String,
+        response: String
+        ,
+        'A step': step((ctx) => {
+          ctx.ret.response3 = ctx.req.param2 + 1
+          return Ok()
+        })
+      })
+      return uc
+    }
+
+    it('should generate some doc', () => {
+      //given
+      usecases.push({ usecase: givenTheSimplestUseCaseWithRequestResponse(), tags: { group: 'SimplestestUseCaseWithRequestResponse' } },)
+      var shelf = renderShelfHTML(usecases)
+      //then
+      assert.ok(shelf)
+    })
+
+    it('should validate if documentation generated is a valid HTML document', () => {
+      //given
+      var shelf = renderShelfHTML(usecases)
+      //then
+      assert.strictEqual(/<(?=.*? .*?\/ ?>|br|hr|input|!--|wbr)[a-z]+.*?>|<([a-z]+).*?<\/\1>/i.test(shelf), true)
+
+    })
+
+  })
 })
 
