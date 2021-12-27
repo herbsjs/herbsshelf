1802056b4b63027decd76053292f01df19c7dd5a
vxksf <kelvin.felix2502@outlook.com>
2021-12-27 20:51:34 -0300

    refactor: using map function instead of for in shelf formatUseCaseDoc

--- src/shelf.js
+++ src/shelf.js
@@ -24,11 +24,11 @@ const formatUseCaseDoc = (usecase) => {
     if (Object.entries(usecase.request).length == 0)
       responseParams.push({ name: 'Object of', type: usecase.request.name })
     else {
-      for (const [key, value] of Object.entries(usecase.request)) {
+      Object.entries(usecase.request).map(([key, value]) => {
         requestParams.push({ 
           name: key, type: value.name ? value.name : { iterableKind: 'Array of', valueOf: value[0].name } 
         })
-      }
+      })
     }
     usecase.request = requestParams
   }
@@ -37,9 +37,9 @@ const formatUseCaseDoc = (usecase) => {
     if (Object.entries(usecase.response).length == 0)
       responseParams.push({ name: 'Object of', type: usecase.response.name })
     else {
-      for (const [key, value] of Object.entries(usecase.response)) {
-        responseParams.push({ 'name': key == '0' ? 'Array of' : key, 'type': value.name })
-      }
+      Object.entries(usecase.response).map(([key, value]) => {
+        responseParams.push({ name: key == '0' ? 'Array of' : key, type: value.name })
+      })
     }
     usecase.response = responseParams
   }
