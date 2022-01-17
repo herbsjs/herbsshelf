5224d0bd4fcb47f750bcf22cbdd1be15a5b8fd29
jhonathanmarolo@hotmail.com <jhonathanmarolo@hotmail.com>
2022-01-17 14:36:49 -0300

    fix: fix if else representation
    re #16

--- src/template/components/stepsCard.js
+++ src/template/components/stepsCard.js
@@ -3,16 +3,19 @@ const StepsCard =`
     <h4 className="icon i-play">Steps:</h4>
     <ul className="steps-list">
       {selectedPage.steps.map(step => step.type === 'if else' ?
-        <ul className="if-steps-list">
-          <li>[if] {step.if.description}</li>
-          <li>[then] {step.then.description}</li>
-          <li>[else] {step.else.description}</li>
+       
+       <div class="step-if-description">
+         <li class="li-if-description">{step.description}</li>
+         <ul className="if-steps-list">
+          <li class="icon i-if">[if] {step.if.description}</li>
+          <li class="icon i-then">[then] {step.then.description}</li>
+          <li class="icon i-then">[else] {step.else.description}</li>
         </ul>
+        </div>
       :
         <li>{step.description}</li>
       )}
     </ul>
   </div>
 `
-
 module.exports = StepsCard
